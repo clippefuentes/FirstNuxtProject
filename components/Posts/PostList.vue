0e93bd031fa9b5823672faa44967ffd1c3fca103
clippefuentes <clippefuentes@gmail.com>
2021-12-29 19:29:02 -0700

    update project

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="post-list">
+    <PostPreview
+      id="1"
+      :is-admin="isAdmin"
+      thumbnail="https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+      title="This is a really cool post!"
+      previewText="Cool Post"
+    />
+    <PostPreview
+      id="2"
+      :is-admin="isAdmin"
+      thumbnail="https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+      title="This is a really cool post part 2!"
+      previewText="Cool Post 2"
+    />
+    <PostPreview
+      id="3"
+      :is-admin="isAdmin"
+      thumbnail="https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+      title="This Third!"
+      previewText="Cool Post beybey"
+    />
+  </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview.vue'
+export default {
+  name: 'PostList',
+  components: {
+    PostPreview
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  }
+};
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ pages/posts/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="posts-page">
+    <PostList />
+  </div>
+</template>
+
+<script lang="ts">
+import PostPreview from '@/components/Posts/PostPreview.vue'
+import PostList from '@/components/Posts/PostList.vue'
+
+export default {
+  components: {
+    PostPreview,
+    PostList,
+  },
+};
+</script>
+
+<style scoped>
+.posts-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
